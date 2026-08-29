461d30c1809589ffbf0ef8a8adfefcb9416da369
Florentino Cazé <florentinocasn@gmail.com>
2026-08-29 14:02:55 -0300

    feat(footer): adiciona o conteúdo inicial do footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import HeroesList from './components/HeroesList'
 import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
       <Main />
       <HeroesList />
+      <Footer />
     </>
   )
 }
