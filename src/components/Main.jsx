e681b0ab349a121749e3cb145dda790c97644795
Florentino Cazé <florentinocasn@gmail.com>
2026-08-30 12:55:48 -0300

    style: adiciona estilo da main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,15 @@ import MainImage from "../assets/main-image.jpg"
 
 function Main() {
     return (
-        <main>
-            <img className="main-image" src={MainImage} />
+        <main className="flex flex-col justify-center items-center w-full h-screen border border-dark-grey">
+            <img className="main-image absolute -z-1 w-screen h-screen" src={MainImage} />
 
-            <div className="main-container">
-                <h1>OVERWATCH HEROES</h1>
+            <div className="main-container w-4xl h-3/4 gap-16 flex flex-col text-center justify-center items-center rounded-sm bg-light-grey-3">
+                <h1 className="text-white text-6xl font-extrabold text-shadow-sm text-shadow-orange">OVERWATCH HEROES</h1>
                 
-                <div className="overwatch-links">
-                    <a href="https://overwatch.blizzard.com/" target="_blank" rel="noopener noreferrer">JOGUE OVERWATCH 2</a>
-                    <a href="https://overwatch.fandom.com/wiki/Overwatch_2" target="_blank" rel="noopener noreferrer">OVERWATCH WIKI</a>
+                <div className="overwatch-links flex flex-row gap-16">
+                    <a className="p-4 bg-orange text-white text-xl font-extrabold text-shadow-md shadow-md rounded-sm hover:scale-105 transition-transform duration-300" href="https://overwatch.blizzard.com/" target="_blank" rel="noopener noreferrer">JOGUE OVERWATCH 2</a>
+                    <a className="p-4 bg-orange text-white text-xl font-extrabold text-shadow-md shadow-md rounded-sm hover:scale-105 transition-transform duration-300" href="https://overwatch.fandom.com/wiki/Overwatch_2" target="_blank" rel="noopener noreferrer">OVERWATCH WIKI</a>
                 </div>
             </div>
         </main>
