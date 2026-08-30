99d6074c9d1e8c31be8c44b43df626a048889388
Florentino Cazé <florentinocasn@gmail.com>
2026-08-30 16:01:41 -0300

    feat(footer): adiciona número na seção de contato

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ function Footer() {
           <a href="https://www.blizzard.com/" className="hover:text-white hover:underline" target="_blank" rel="noopener noreferrer">Blizzard</a>
         </section>
 
-        <section className="footer-section">
+        <section className="footer-section mb-8 lg:mb-0">
           <h3 className="mb-4 text-lg font-bold text-white">Contato</h3>
 
-          <div className="social-icons flex flex-row gap-8 mb-8 lg:mb-0">
+          <div className="social-icons flex flex-row gap-8 mb-4 lg:mb-4">
             <a href="mailto:florentinocaze@gmail.com" className="social-links">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="ionicon w-10 hover:text-white hover:scale-110 transition-transform duration-300"><rect width="416" height="320" x="48" y="96" rx="40" ry="40" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32px"/><path d="m112 160 144 112 144-112" fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32px"/></svg>
             </a>
@@ -32,6 +32,8 @@ function Footer() {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="ionicon w-10 hover:text-white hover:scale-110 transition-transform duration-300"><path fill="currentColor" d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.6 17.6 0 0 0 3.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 0 1-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0 0 25.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.6 18.6 0 0 1 5-.5c8.1 0 26.4 3.1 56.6 24.1a208.2 208.2 0 0 1 112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.6 18.6 0 0 1 5 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.4 19.4 0 0 0 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32"/></svg>
             </a>
           </div>
+
+          <small>Telefone: +55 (83) 9 9151-7024</small>
         </section>
       </div>
       
